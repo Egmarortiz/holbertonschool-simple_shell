992fa5e1e473569f4b540818e1a5f0e57c82547c
Egmarortiz <egmarocasio9@gmail.com>
2025-04-23 22:37:30 -0400

    exit implementation in main.c

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
- * main - entry point for simple_shell with args support
+ * main - entry point for simple_shell with args & exit built-in
  * @argc: arg count (unused)
- * @argv: arg vector; argv[0] used in errors
+ * @argv: arg vector; argv[0] used for errors
  * Return: 0 on exit
  */
 int main(int argc, char **argv)
@@ -30,6 +31,12 @@ int main(int argc, char **argv)
 		args = split_line(line);
 		if (args)
 		{
+			if (strcmp(args[0], "exit") == 0)
+			{
+				free_args(args);
+				free(line);
+				exit(0);
+			}
 			execute_command(args, argv[0]);
 			free_args(args);
 		}
